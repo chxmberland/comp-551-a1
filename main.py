78f4d63f7bc6a8bc22edecc66d0c37840f63fe20
Benjamin Chamberland <benjamin.chamberland@goquant.io>
2024-01-23 17:36:47 -0500

    Task: Pulling in data set 2

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import pandas as pd
 
-nhanes = pd.read_csv("NHANES_age_prediction.csv")
-print(nhanes)
+# DATASET 1
+nhanes = pd.read_csv("dataset1/NHANES_age_prediction.csv")
+
+# DATASET 2
+# __________________________________________
+
+# Subset 1: Breast cancer diagnoses
+# Columns
+# 1. Sample code number            id number
+# 2. Clump Thickness               1 - 10
+# 3. Uniformity of Cell Size       1 - 10
+# 4. Uniformity of Cell Shape      1 - 10
+# 5. Marginal Adhesion             1 - 10
+# 6. Single Epithelial Cell Size   1 - 10
+# 7. Bare Nuclei                   1 - 10
+# 8. Bland Chromatin               1 - 10
+# 9. Normal Nucleoli               1 - 10
+# 10. Mitoses                      1 - 10
+# 11. Class:                       (2 for benign, 4 for malignant)
+breast_cancer_wisconsin = pd.read_csv("dataset2/breast-cancer-wisconsin.csv")
