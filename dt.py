fb383ef6b75ab0855b5523ae518af0cab51c0cb7
Geoffrey Meric <gcs.meric@gmail.com>
2024-01-26 15:58:22 -0500

    Update dt.py

--- dt.py
+++ dt.py
@@ -16,3 +16,123 @@ class Node:
             class_prob = np.bincount(self.labels[data_indices], minlength=self.num_classes) #this is counting frequency of different labels in the region defined by this node
             self.class_prob = class_prob / np.sum(class_prob)  #stores the class probability for the node
             #note that we'll use the class probabilites of the leaf nodes for making pr
+
+
+def greedy_test(node, cost_fn):
+    #initialize the best parameter values
+    best_cost = np.inf
+    best_feature, best_value = None, None
+    num_instances, num_features = node.data.shape
+    #sort the features to get the test value candidates by taking the average of consecutive sorted feature values 
+    data_sorted = np.sort(node.data[node.data_indices],axis=0)
+    test_candidates = (data_sorted[1:] + data_sorted[:-1]) / 2.
+    for f in range(num_features):
+        #stores the data corresponding to the f-th feature
+        data_f = node.data[node.data_indices, f]
+        for test in test_candidates[:,f]:
+            #Split the indices using the test value of f-th feature
+            left_indices = node.data_indices[data_f <= test]
+            right_indices = node.data_indices[data_f > test]
+            #we can't have a split where a child has zero element
+            #if this is true over all the test features and their test values  then the function returns the best cost as infinity
+            if len(left_indices) == 0 or len(right_indices) == 0:                
+                continue
+            #compute the left and right cost based on the current split                                                         
+            left_cost = cost_fn(node.labels[left_indices])
+            right_cost = cost_fn(node.labels[right_indices])
+            num_left, num_right = left_indices.shape[0], right_indices.shape[0]
+            #get the combined cost using the weighted sum of left and right cost
+            cost = (num_left * left_cost + num_right * right_cost)/num_instances
+            #update only when a lower cost is encountered
+            if cost < best_cost:
+                best_cost = cost
+                best_feature = f
+                best_value = test
+    return best_cost, best_feature, best_value
+
+
+#computes misclassification cost by subtracting the maximum probability of any class
+def cost_misclassification(labels):
+    counts = np.bincount(labels) 
+    class_probs = counts / np.sum(counts)
+    #you could compress both the steps above by doing class_probs = np.bincount(labels) / len(labels)
+    return 1 - np.max(class_probs)
+
+#computes entropy of the labels by computing the class probabilities
+def cost_entropy(labels):
+    class_probs = np.bincount(labels) / len(labels)
+    class_probs = class_probs[class_probs > 0]              #this steps is remove 0 probabilities for removing numerical issues while computing log
+    return -np.sum(class_probs * np.log2(class_probs))       #expression for entropy -\sigma p(x)log[p(x)]
+
+#computes the gini index cost
+def cost_gini_index(labels):
+    class_probs = np.bincount(labels) / len(labels)
+    return 1 - np.sum(np.square(class_probs))               #expression for gini index 1-\sigma p(x)^2
+
+
+
+class DecisionTree:
+    def __init__(self, num_classes=None, max_depth=3, cost_fn=cost_entropy, min_leaf_instances=1):
+        self.max_depth = max_depth      #maximum dept for termination 
+        self.root = None                #stores the root of the decision tree 
+        self.cost_fn = cost_fn          #stores the cost function of the decision tree 
+        self.num_classes = num_classes  #stores the total number of classes
+        self.min_leaf_instances = min_leaf_instances  #minimum number of instances in a leaf for termination
+        
+    def fit(self, data, labels):
+        self.data = data
+        self.labels = labels
+        if self.num_classes is None:
+            #self.num_classes = np.max(labels) + 1
+            self.num_classes = len(np.unique(labels))
+        #below are initialization of the root of the decision tree
+        self.root = Node(np.arange(data.shape[0]), None)
+        self.root.data = data
+        self.root.labels = labels
+        self.root.num_classes = self.num_classes
+        self.root.depth = 0
+        #to recursively build the rest of the tree
+        self._fit_tree(self.root)
+        return self
+
+    def _fit_tree(self, node):
+        #This gives the condition for termination of the recursion resulting in a leaf node
+        if node.depth == self.max_depth or len(node.data_indices) <= self.min_leaf_instances:
+            return
+        #greedily select the best test by minimizing the cost
+        cost, split_feature, split_value = greedy_test(node, self.cost_fn)
+        #if the cost returned is infinity it means that it is not possible to split the node and hence terminate
+        if np.isinf(cost):
+            return
+        #print(f'best feature: {split_feature}, value {split_value}, cost {cost}')
+        #to get a boolean array suggesting which data indices corresponding to this node are in the left of the split
+        test = node.data[node.data_indices,split_feature] <= split_value
+        #store the split feature and value of the node
+        node.split_feature = split_feature
+        node.split_value = split_value
+        #define new nodes which are going to be the left and right child of the present node
+        left = Node(node.data_indices[test], node)
+        right = Node(node.data_indices[np.logical_not(test)], node)
+        #recursive call to the _fit_tree()
+        self._fit_tree(left)
+        self._fit_tree(right)
+        #assign the left and right child to present child
+        node.left = left
+        node.right = right
+
+    def predict(self, data_test):
+        class_probs = np.zeros((data_test.shape[0], self.num_classes))
+        for n, x in enumerate(data_test):
+            node = self.root
+            #loop along the dept of the tree looking region where the present data sample fall in based on the split feature and value
+            while node.left:
+                if x[node.split_feature] <= node.split_value:
+                    node = node.left
+                else:
+                    node = node.right
+            #the loop terminates when you reach a leaf of the tree and the class probability of that node is taken for prediction
+            class_probs[n,:] = node.class_prob
+        return class_probs
+    
+    
+
